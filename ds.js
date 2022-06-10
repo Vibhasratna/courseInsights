61b984483fc7e072ce2ad1ad57c902b5acee0709
Vibhas Ratna <100060723+Vibhasratna@users.noreply.github.com>
2022-06-10 08:20:56 +0530

    Add files via upload

---
+++ ds.js
@@ -0,0 +1,21 @@
+
+document.getElementById("firstBtn").addEventListener("click", first);
+function first() {
+  window.location.href="info.html";
+}
+
+document.getElementById("secBtn").addEventListener("click", second);
+function second() {
+  window.location.href="info-2.html";
+}
+
+document.getElementById("thirdBtn").addEventListener("click", third);
+function third() {
+  window.location.href="info-3.html";
+}
+
+document.getElementById("forthBtn").addEventListener("click", forth);
+function forth() {
+  window.location.href="info-4.html";
+}
+
